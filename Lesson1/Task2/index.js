a51a9f3c81abc9a7ae8d1bb142db4dc8a4b7f053
GrinyaNet <igrinya@yahoo.com>
2024-06-11 12:38:36 +0300

    Task2

--- Lesson1/Task2/index.js
+++ Lesson1/Task2/index.js
@@ -2,19 +2,17 @@ const rootElement = document.querySelector('#root');
 
 const element = React.createElement(
     'div',
-    { className: 'greeting'},
-    [
-        React.createElement(
-            'h1',
-            { className: 'greeting__title'},
-            'Hello, world!'
-        ),
-        React.createElement(
-            'p',
-            { className: 'greeting__text'},
-            "I'm learning React"
-        ),
-    ]
+    { className: 'greeting' },
+    React.createElement(
+        'h1',
+        { className: 'greeting__title' },
+        'Hello, world!'
+    ),
+    React.createElement(
+        'p',
+        { className: 'greeting__text' },
+        "I'm learning React"
+    ),
 );
 
 ReactDOM.render(element, rootElement);
