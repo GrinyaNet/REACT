34707ba88b8a98659fb7712520317a0e74055446
GrinyaNet <igrinya@yahoo.com>
2024-06-12 13:56:40 +0300

    Task3

--- Lesson1/Task3/webpack.config.js
+++ Lesson1/Task3/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {    
@@ -13,9 +15,14 @@ module.exports = {
                 }
             ]
         },
+        output: {
+            path: path.resolve(__dirname, 'review_build')            
+          },
         plugins: [            
             new HtmlWebpackPlugin({
                 template: './src/index.html'
             })
-        ]        
+        ],
+         mode: 'production'
+        
 };
