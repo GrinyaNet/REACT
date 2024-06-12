f7c0ec69ac2cc975512b18ccef50357c906f70ce
GrinyaNet <igrinya@yahoo.com>
2024-06-12 09:56:01 +0300

    Task3

--- Lesson1/Task3/webpack.config.js
+++ Lesson1/Task3/webpack.config.js
@@ -1,15 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
-module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production';
-    const config = {
-        entry: './src/index.js',
-        output: {
-            filename: 'bundle.js'
-        },
+module.exports = {    
         module: {
             rules: [
                 {
@@ -17,45 +8,14 @@ module.exports = (env, argv) => {
                     use: ['babel-loader']
                 },
                 {
-                    test: /.s?css$/,
-                    use: [isProduction
-                        ? MiniCssExtractPlugin.loader
-                        : 'style-loader',
-                        'css-loader',
-                        'sass-loader',
-                    ]
-                },
-                {
-                    test: /.(jpg|png)$/,
-                    use: [
-                        {
-                            loader: 'url-loader',
-                            options: {
-                                limit: 8192,
-                                name: '[name].[ext]',
-                                outputPath: 'images',
-                            },
-                        },
-                    ],
+                    test: /.css$/,
+                    use: ['style-loader', 'css-loader']
                 }
-            ],
+            ]
         },
-        plugins: [
-            new webpack.ProgressPlugin(),
-            new CleanWebpackPlugin(),
+        plugins: [            
             new HtmlWebpackPlugin({
                 template: './src/index.html'
-            }),            
-        ],
-        devServer: {
-            port: 9000,
-            hot: true,
-        }
-    };
-    if (isProduction) {
-        config.plugins.push(new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }));
-    }
-    return config;
+            })
+        ]        
 };
