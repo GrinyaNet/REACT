197481563fb619067f9fba094dd72cc2ae4c86cb
GrinyaNet <igrinya@yahoo.com>
2024-06-15 16:03:36 +0300

    Task5

--- Lesson3/Task5/src/Profile.jsx
+++ Lesson3/Task5/src/Profile.jsx
@@ -13,7 +13,7 @@ const formatDate = date => moment(date).format('DD MMM YYYY');
   return (
     <>
     <h1 className="profile__name">{userData.user.firstName} {userData.user.lastName}</h1>
-    <h1 className="profile__birth">{'Was born'} {formatDate(userData.date)} {userData.user.birthPlace}</h1>
+    <h1 className="profile__birth">{'Was born'} {formatDate(userData.date)} {'in'} {userData.user.birthPlace}</h1>
   </>
   );
 };
