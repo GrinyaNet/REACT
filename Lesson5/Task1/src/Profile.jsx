8b03621462efe1ffa50c921b8dc3ded274f3432f
GrinyaNet <igrinya@yahoo.com>
2024-06-18 09:04:54 +0300

    Task1

---
+++ Lesson5/Task1/src/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import moment from 'moment';
+
+
+const formatDate = date => moment(date).format('DD MMM YY');
+
+ const UserData = (props) => {
+  return (
+    <>
+    <p className="profile__name">{props.userData.firstName} {props.userData.lastName}</p>
+    <p className="profile__birth">{'Was born'} {formatDate(props.userData.birthDate)} {'in'} {props.userData.birthPlace}</p>
+  </>
+  );
+};
+  
+export default UserData;
---
+++ Lesson5/Task1/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import UserData from './Profile';
+
+
+
+const rootElement = document.querySelector('#root');
+
+const userData = {
+    firstName: 'James',
+    lastName: 'Bond',
+    birthDate: '1991-01-17T11:11:11.819Z',
+    birthPlace: 'London',
+  };
+
+//ReactDOM.render(<UserData userData={userData} birthDate={userData.birthDate}/>, rootElement);
+ReactDOM.render(<UserData userData={userData} />, rootElement);
