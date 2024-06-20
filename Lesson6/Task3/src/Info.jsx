afedb2cc307ecbccfffaf50e18c536a6e181bfa9
GrinyaNet <igrinya@yahoo.com>
2024-06-20 11:14:59 +0300

    Task3

---
+++ Lesson6/Task3/src/Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Info = ({text}) => {
+    if (!text)
+        {
+            return null;
+        }
+return (
+    <div className="message">{text}</div>
+);
+};
+
+export default Info;
