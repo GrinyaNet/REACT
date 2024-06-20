022ea7779e34b91da3a9fcc4b455957b6328ab89
GrinyaNet <igrinya@yahoo.com>
2024-06-20 15:48:05 +0300

    Task4

---
+++ Lesson6/Task4/src/Status.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Online from "./Online.jsx";
+import Offline from "./Offline.jsx";
+
+const Status = props => {
+    if (props.isOnline) {
+        return (
+            <Online />
+        );
+    }
+    return <Offline />;
+};
+
+export default Status;
