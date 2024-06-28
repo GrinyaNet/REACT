8ffa4ec130ae425405ea52b19d9486c5d82c6f60
GrinyaNet <igrinya@yahoo.com>
2024-06-28 10:44:36 +0300

    Task4

--- Lesson8/Task4/src/User.jsx
+++ Lesson8/Task4/src/User.jsx
@@ -1,27 +1,45 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class User extends Component {
-    state = {
-        user: null,
-    }
-    render() {
-        return (
-<div className="user">  
-  <img
-    alt="User Avatar"
-    src="https://avatars3.githubusercontent.com/u/69631?v=4"
-    className="user__avatar"
-  />
-  <div className="user__info">
-    
-    <span className="user__name">{this.state.user.name}</span>
-    
-    <span className="user__location">{this.state.user.location}</span>
-  </div>
-</div>
+  state = {
+    user: null,
+  };
 
-        );
+  componentDidMount() {
+    this.fetchUser(this.props.userId);
+  }
+
+  fetchUser = userId => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          user: data,
+        });
+      });
+  };
+
+  render() {
+    const { user } = this.state;
+    if (!user) {
+      return null;
     }
+    const { name, location } = user;
+    return (
+      <div className="user">
+        <img
+          alt="User Avatar"
+          src="https://avatars3.githubusercontent.com/u/69631?v=4"
+          className="user__avatar"
+        />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+
+          <span className="user__location">{location}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default User;
