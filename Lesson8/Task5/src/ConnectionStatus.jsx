9ebae24f673d24a389086ce688076f0bc4513955
GrinyaNet <igrinya@yahoo.com>
2024-06-29 17:49:41 +0300

    Task5

--- Lesson8/Task5/src/ConnectionStatus.jsx
+++ Lesson8/Task5/src/ConnectionStatus.jsx
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class ConnectionStatus extends Component {
   state = {
     status: 'online',
-    classStyle: 'status',
+    //classStyle: 'status',
   };
 
   componentDidMount() {
     window.addEventListener('offline', this.updateOflineStatus);
     window.addEventListener('online', this.updateOnlineStatus);
-    this.updateOflineStatus();
-    this.updateOnlineStatus();
+    //this.updateOflineStatus();
+    //this.updateOnlineStatus();
   }
 
   componentWillUnmount() {
@@ -21,19 +21,23 @@ class ConnectionStatus extends Component {
   updateOflineStatus = () => {
     this.setState({
       status: 'offline',
-      classStyle: 'status_offline',
+      //classStyle: 'status_offline',
     });
   };
 
   updateOnlineStatus = () => {
     this.setState({
       status: 'online',
-      classStyle: 'status',
+      //classStyle: 'status',
     });
   };
 
   render() {
-    const { status, classStyle } = this.state;
+    const { status } = this.state;
+    let classStyle = 'status';
+    if (status === 'offline') {
+      return <div className="status status_offline">{status}</div>;
+    }
 
     return <div className={classStyle}>{status}</div>;
   }
