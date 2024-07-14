275bc2f480b3f803fb7211f5bf6beab76c6bba84
GrinyaNet <igrinya@yahoo.com>
2024-07-14 12:59:24 +0300

    Task3

--- Lesson11/Task3/src/App.jsx
+++ Lesson11/Task3/src/App.jsx
@@ -6,32 +6,26 @@ class App extends Component {
     isOpen: false,
   };
 
-  // hideDialog = () => {
-  //   this.setState({
-  //     isOpen: false,
-  //   });
-  // };
-
   showDialog = () => {
     if (!this.state.isOpen) {
-    this.setState({
-      isOpen: true,
-    });
-  } else {
-    this.setState({
-      isOpen: false,
-    });
-  }
+      this.setState({
+        isOpen: true,
+      });
+    } else {
+      this.setState({
+        isOpen: false,
+      });
+    }
   };
 
   render() {
     return (
-      <div className="app">        
+      <div className="app">
         <Expand isOpen={this.state.isOpen} onClose={this.showDialog} title="Some title">
-        <p>
-          Hooks are a new addition in React 16.8. They let you use state and other React features
-          without writing a class.
-        </p>
+          <p>
+            Hooks are a new addition in React 16.8. They let you use state and other React features
+            without writing a class.
+          </p>
         </Expand>
       </div>
     );
--- Lesson11/Task3/src/Expand.jsx
+++ Lesson11/Task3/src/Expand.jsx
@@ -1,52 +1,32 @@
 import React from 'react';
 
 const Expand = ({ isOpen, children, title, onClose }) => {
-  const element = "";
+  
   if (isOpen) {
-     const element = (
+    const element = (
       <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={onClose}>
-        <i className="fa fa-chevron-up" aria-hidden="true"></i>
-        </button>
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={onClose}>
+            <i className="fa fa-chevron-up" aria-hidden="true"></i>
+          </button>
+        </div>
+        <div className="expand__content">{children}</div>
       </div>
-      <div className="expand__content">
-        {children}
-      </div>
-    </div>
-    )
+    );
     return element;
   }
-  
-  // if (!isOpen) {
-  //   const element = (
-  //     <i className="fas fa-chevron-down"></i>
-  //   )
-  //   return element;
-  // }
-  // } else {
-  //   const element = (
-  //     <i className="fas fa-chevron-up"></i>
-  //   )
-  //   return element;
-  // }
 
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button className="expand__toggle-btn" onClick={onClose}>
-        <i className="fa fa-chevron-down" aria-hidden="true"></i>
+          <i className="fa fa-chevron-down" aria-hidden="true"></i>
         </button>
       </div>
-      {/* <div className="expand__content">
-        {children}
-      </div> */}
     </div>
   );
 };
 
 export default Expand;
-//<i class="fa fa-chevron-down" aria-hidden="true"></i>
-//<i className="fas fa-chevron-up"></i>
