3d3f56601dcafd6e291a1d73d1e82383559a1112
GrinyaNet <igrinya@yahoo.com>
2024-07-18 10:32:06 +0300

    Task1

---
+++ Lesson12/Task1/src/taskGateway.js
@@ -0,0 +1,49 @@
+const baseUrl = 'https://669781f802f3150fb66de387.mockapi.io/users';
+
+export const createTask = taskData => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;utc-8',
+    },
+    body: JSON.stringify(taskData),
+  }).then(responce => {
+    if (!responce.ok) {
+      throw new Error('Faild to create task');
+    }
+  });
+};
+
+export const fetchTasksList = () => {
+  return fetch(baseUrl)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(tasksList => tasksList.map(({ _id, ...task }) => ({ id: _id, ...task })));
+};
+
+export const updateTask = (taskId, taskTada) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;utc-8',
+    },
+    body: JSON.stringify(taskTada),
+  }).then(responce => {
+    if (!responce.ok) {
+      throw new Error('Faild to create task');
+    }
+  });
+};
+
+export const deleteTask = taskId => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  }).then(responce => {
+    if (!responce.ok) {
+      throw new Error('Failed to delete task');
+    }
+  });
+};
