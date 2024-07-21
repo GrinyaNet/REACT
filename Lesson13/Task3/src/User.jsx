ffbdac094b45bc12f962480df1c9f5b422105a76
GrinyaNet <igrinya@yahoo.com>
2024-07-21 10:37:41 +0300

    Task3

--- Lesson13/Task3/src/User.jsx
+++ Lesson13/Task3/src/User.jsx
@@ -11,8 +11,10 @@ function User({ match }) {
         <span className="user__name">GitHub</span>
         <span className="user__location">San Francisco,CA</span>
       </div>
-    {/* {match.params.productId} */}
+    {match.params.userId}
     </div>;
 }
 
 export default User;
+
+//https://api.github.com/users/USER_ID
