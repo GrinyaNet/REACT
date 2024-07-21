286b3be1df492a40710efbaad99e0180d41986a8
GrinyaNet <igrinya@yahoo.com>
2024-07-21 13:06:58 +0300

    Task2

---
+++ Lesson14/Task2/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App.jsx';
+//import Counter from './Counter.jsx';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<App />, rootElement);
