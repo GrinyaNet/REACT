df75431aff7fddcc443735fb6716b0cb4aefcc42
GrinyaNet <igrinya@yahoo.com>
2024-07-22 11:45:39 +0300

    Task4

--- Lesson14/Task4/src/ConnectionStatus.jsx
+++ Lesson14/Task4/src/ConnectionStatus.jsx
@@ -1,46 +1,32 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-class ConnectionStatus extends Component {
-  state = {
-    status: 'online',
-    //classStyle: 'status',
-  };
+const ConnectionStatus = () => {
+  const [status, setStatus] = useState('online');
 
-  componentDidMount() {
-    window.addEventListener('offline', this.updateOflineStatus);
-    window.addEventListener('online', this.updateOnlineStatus);
-    //this.updateOflineStatus();
-    //this.updateOnlineStatus();
+  useEffect(() => {
+    const updateOflineStatus = () => {
+      setStatus('offline');
+    };
+
+    const updateOnlineStatus = () => {
+      setStatus('online');
+    };
+
+    window.addEventListener('offline', updateOflineStatus);
+    window.addEventListener('online', updateOnlineStatus);
+
+    return () => {
+      window.removeEventListener('offline', updateOflineStatus);
+      window.removeEventListener('online', updateOnlineStatus);
+    };
+  }, []);
+
+  let classStyle = 'status';
+  if (status === 'offline') {
+    return <div className="status status_offline">{status}</div>;
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('offline', this.updateOflineStatus);
-    window.removeEventListener('online', this.updateOnlineStatus);
-  }
-
-  updateOflineStatus = () => {
-    this.setState({
-      status: 'offline',
-      //classStyle: 'status_offline',
-    });
-  };
-
-  updateOnlineStatus = () => {
-    this.setState({
-      status: 'online',
-      //classStyle: 'status',
-    });
-  };
-
-  render() {
-    const { status } = this.state;
-    let classStyle = 'status';
-    if (status === 'offline') {
-      return <div className="status status_offline">{status}</div>;
-    }
-
-    return <div className={classStyle}>{status}</div>;
-  }
-}
+  return <div className={classStyle}>{status}</div>;
+};
 
 export default ConnectionStatus;
